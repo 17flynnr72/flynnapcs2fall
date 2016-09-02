04801d6b2d3f6727f7c425a4d7a14e874791b242
17flynnr72 <17flynnr72@millshighsch.org>
2016-09-02 09:48:16 -0700

    starting part 2 of caluclations

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -15,5 +15,25 @@ public class Calculate {
 		answer=num+num/2;
 		return (answer);
 	}
-	public static int average()
+	public static int average(int num1, int num2, int num3){
+		int answer;
+		answer=num1+num2+num3/3;
+		return (answer);
+	}
+	public static double toDegrees(double num){
+		double answer;
+		answer=num*3.14159/180;
+		return (answer);
+	}
+	public static double toRadians(double num){
+		double answer;
+		answer=num*180/3.14159;
+		return (answer);
+	}
+	public static double discriminant(double num1, double num2, double num3){
+		double answer;
+		answer=num2*num2-4*num1*num3;
+		return (answer);
+	}
+	
 }
