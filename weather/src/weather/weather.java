e785babed5461a0376ae4d01d86c379b50e5c3a7
17flynnr72 <17flynnr72@millshighsch.org>
2017-02-23 09:13:50 -0800

    yo dawg, we got that weather thing you did now(kinda)

--- weather/src/weather/weather.java
+++ weather/src/weather/weather.java
@@ -1,5 +1,14 @@
 package weather;
 
-public class weather {
+import java.io.*;
+import java.util.*;
 
+public class WeatherDiff{
+	public static void main (String [] args) throws FileNotFoundException{
+		Scanner input = new Scanner (new File("./data/weather.txt"));
+		String text = input.next();
+		System.out.println(text);
+	}
+
+	
 }
